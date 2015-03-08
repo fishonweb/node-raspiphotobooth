3ab6fba45dcdeef0a77d50751181aade0508e8c9
julien marez <julien.marez@gmail.com>
2015-03-08 17:35:46 +0100

    update server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var app = express()
 
+
+app.use(express.static('pics'));
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/pics', function (req, res) {
-  app.use(express.static('pics'));
-})
+
 
 app.listen(3000)
