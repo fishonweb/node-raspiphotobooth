fa764c318378123c57f1c0532c485069266e6dbf
wrench13 <julien.marez@gmail.com>
2015-03-08 13:45:40 +0000

    init npm + some test

---
+++ app.js
@@ -0,0 +1,40 @@
+
+var RaspiCam = require("raspicam");
+
+var outputPath = "./pics/pic%d.jpg";
+
+var camera = new RaspiCam({ 
+  mode : "photo",
+  width : 1024,
+  height : 768,
+  output : outputPath,
+  quality : 10
+});
+
+var keypress = require('keypress');
+
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  if (key && key.ctrl && key.name == 'c') {
+    process.exit();
+  }
+  if(key.name == "return"){
+    camera.start()
+  }
+  
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+//to take a snapshot, start a timelapse or video recording
+//camera.start( );
+
+//to stop a timelapse or video recording
+/*setTimeout(function() {
+  camera.stop();
+}, 800)*/
+
