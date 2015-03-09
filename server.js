1eeec41f588b029a3ff5b9032a52c25de2ccfa24
julien marez <julien.marez@gmail.com>
2015-03-09 22:36:29 +0100

    update server

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ var app = express()
 app.use('/pics', express.static('pics'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('<img src="./pics/pic1.jpg" />')
 })
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
+console.log(getRandomInt(2, 10))
 
 app.listen(3000)
