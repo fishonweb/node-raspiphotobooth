e90de3f1d72ed810fe8bf7ca0d7bddde3a985fc7
julien marez <julien.marez@gmail.com>
2015-03-09 23:04:41 +0100

    fix

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 
+//gpio var
+var led1_pin = 15,
+    led2_pin = 19,
+    led3_pin = 21,
+    led4_pin = 23,
+    ledBtn_pin = 11,
+    button1_pin = 22,
+    button2_pin = 18,
+    button3_pin = 16;
+
+    var gpio = require('rpi-gpio');
+
+    gpio.setup(ledBtn_pin, gpio.DIR_IN, readInput);
+
+    function readInput() {
+        gpio.read(7, function(err, value) {
+            console.log('The value is ' + value);
+        });
+    }
+
 var RaspiCam = require("raspicam");
 var count = 0
 
@@ -26,6 +46,7 @@ process.stdin.on('keypress', function (ch, key) {
   }
   //Listen for enter keypress and start camera
   if(key.name == "return"){
+    outputPath = "./pics/pic" + count + ".jpg";
     var options = {
       mode : "photo",
       width : 1024,
