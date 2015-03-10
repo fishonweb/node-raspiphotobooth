68ecccafe8af5f733cc3c82db9d201eb3894f57c
wrench13 <julien.marez@gmail.com>
2015-03-10 20:33:58 +0100

    update

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
 
-//gpio var
-var led1_pin = 15,
-    led2_pin = 19,
-    led3_pin = 21,
-    led4_pin = 23,
-    ledBtn_pin = 11,
-    button1_pin = 22,
-    button2_pin = 18,
-    button3_pin = 16;
-
-    var gpio = require('rpi-gpio');
-
-    gpio.setup(ledBtn_pin, gpio.DIR_IN, readInput);
-
-    function readInput() {
-        gpio.read(7, function(err, value) {
-            console.log('The value is ' + value);
-        });
-    }
 
 var RaspiCam = require("raspicam");
 var count = 0
