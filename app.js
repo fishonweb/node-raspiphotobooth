88cdd0320b000055fa935f0bd4c5b6627abbb6cd
wrench13 <julien.marez@gmail.com>
2015-03-11 00:08:13 +0100

    fix

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ camera.on("exit", function(){
   //console.log(count)
   if(count < 2){
     count++
-    return takePic(count)
+    camera.start()
   } else {
     tile(path, input, tileOutputPath, tileOutputName)
     count = 1
