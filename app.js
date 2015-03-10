96a6875d8edee343beceaf09d726dbc31c4ac4c3
wrench13 <julien.marez@gmail.com>
2015-03-10 23:11:11 +0100

    fix

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ process.stdin.on('keypress', function (ch, key) {
 //listen for the process to exit when the timeout has been reached
 camera.on("exit", function(){
   console.log(count)
-  if(count < 2){
+  if(count <= 2){
     count++
     takePic(count)
   } else {
