4b64899ab6c5f07be7da6024627a644cf1c272a5
wrench13 <julien.marez@gmail.com>
2015-03-10 23:23:21 +0100

    fix

--- utils/tileimg.js
+++ utils/tileimg.js
@@ -7,11 +7,11 @@ module.exports = function tile(inputFolder, inputArrayImgs, outputFolder, output
   console.log("tile image")
   gm()
     .in('-page', '+0+0')  // Custom place for each of the images
-    .in(inputFolder + "/" + inputArrayImgs[0])
+    .in(inputFolder + inputArrayImgs[0])
     .in('-page', '+0+780')
-    .in(inputFolder + "/" + inputArrayImgs[1])
+    .in(inputFolder + inputArrayImgs[1])
     .mosaic()  // Merges the images as a matrix
-    .write(outputFolder  + "/" + outputName +".jpg", function (err) {
+    .write(outputFolder  + outputName +".jpg", function (err) {
       if (err) console.log(err);
         console.log(this.outname + " created  ::  " + arguments[3])
     })
