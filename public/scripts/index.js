9b3682d4df2e918819ec43cd0bde3ef169b04c1a
wrench13 <julien.marez@gmail.com>
2015-04-05 10:50:07 +0200

    path pic

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,5 +3,5 @@ var socket = io();
 var carousel = document.getElementById('carousel')
 
 socket.on('photobooth', function(pic){
-  carousel.innerHTML = carousel.innerHTML + '<br/>' + '<img src="photobooth/' + pic + '.jpg"' />'
+  carousel.innerHTML = carousel.innerHTML + '<br/>' + '<img src="photobooth/' + pic + '.jpg" />'
 });
