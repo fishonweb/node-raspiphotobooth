73d2cf9051fbd1dd82b91ae71b1566387fd60407
Julien Marez <julien.marez@utocat.com>
2020-10-25 20:08:57 +0100

    feat: nodemon

--- server.js
+++ server.js
@@ -1,76 +1,91 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var bodyParser = require("body-parser");
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 var levelup = require("levelup");
 var leveldown = require("leveldown");
-var dbParams = levelup(leveldown('./params'));
+var dbParams = levelup(leveldown("./params"));
+var dbPictures = levelup(leveldown('./picsdb'));
 
-app.use('/photobooth', express.static('photobooth'));
-app.use('/bower', express.static('bower_components'));
-app.use(express.static('public/'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.use("/photobooth", express.static("photobooth"));
+app.use(express.static("public/"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get('/admin', function (req, res) {
-  res.sendFile(__dirname + '/public/admin.html');
+app.get("/admin", function (req, res) {
+  res.sendFile(__dirname + "/public/admin.html");
 });
 
-app.get('/api/params', function (req, res) {
+app.get("/pictures", (req, res) => {
+  let stream = [];
+  dbPictures.createValueStream()
+    .on("data", function (data) {
+      stream.push(data.toString('utf8'));
+    })
+    .on("end", function () {
+      res.json(stream);
+    });
+});
+
+app.post("/api/params", function (req, res) {
   var params = {
     title: req.query.title,
-    time: req.query.time
+    time: req.query.time,
   };
-  dbParams.put('params', JSON.stringify(params), function (err) {
-    console.log('sett params');
+  dbParams.put("params", JSON.stringify(params), function (err) {
+    console.log("sett params");
     if (err) {
-      return console.log('Ooops!', err); // some kind of I/O error
+      return console.log("Ooops!", err); // some kind of I/O error
     }
-  })
+  });
   res.json(params);
 });
 
-app.get('/api/getparams', function (req, res) {
-  dbParams.get('params', function (err, value) {
-    if (err) return console.log('Ooops!', err) // likely the key was not found
+app.get("/api/getparams", function (req, res) {
+  dbParams.get("params", function (err, value) {
+    if (err) return console.log("Ooops!", err); // likely the key was not found
 
     // ta da!
     var appParam = JSON.parse(value);
     res.jsonp(appParam);
-  })
-})
-
-io.on('connection', function (socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
-
-  socket.on('photobooth', function (pic) {
-    console.log('socket emit pic : ' + pic);
-    io.emit('photobooth', pic);
-  });
-  socket.on('timer', function (timer) {
-    console.log('socket emit timer');
-    io.emit('timer', timer);
-  });
-  socket.on('random', function (randompics) {
-    console.log('socket emit random');
-    io.emit('random', randompics);
-  });
-  socket.on('start', function (start) {
-    io.emit('start', start);
-  });
-  socket.on('picAgain', function (picAgain) {
-    console.log('pic again');
-    io.emit('picAgain', picAgain);
   });
 });
 
-http.listen(3000, function () {
-  console.log('listening on http://localhost:3000');
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  });
+
+  socket.on("photobooth", function (pic) {
+    console.log("socket emit pic : " + pic);
+    io.emit("photobooth", pic);
+  });
+  socket.on("timer", function (timer) {
+    console.log("socket emit timer");
+    io.emit("timer", timer);
+  });
+  socket.on("random", function (randompics) {
+    console.log("socket emit random");
+    io.emit("random", randompics);
+  });
+  socket.on("start", function (start) {
+    io.emit("start", start);
+  });
+  socket.on("picAgain", function (picAgain) {
+    console.log("pic again");
+    io.emit("picAgain", picAgain);
+  });
+});
+
+http.listen(3009, function () {
+  console.log("listening on http://localhost:3009");
 });
--- utils/tileimg.js
+++ utils/tileimg.js
@@ -1,5 +1,5 @@
 var gm = require('gm')
-var socket = require('socket.io-client')('http://localhost:3000');
+var socket = require('socket.io-client')(`http://localhost:${process.env.SERVER_PORT}`);
 
 module.exports = function tile(inputFolder, inputArrayImgs, outputFolder, outputName, tileNum, callback) {
     // a, b  ->  a b
